41545df00a60218180ec99ca5a2db35134b995f8
Donya Johari <djohari37@gmail.com>
2021-10-28 14:42:10 -0400

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Donya Johari on 2021-10-28.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
