0fabd93d5ebc78c0ff42289d7764e85f5f225e6e
Donya Johari <45051592+joharido@users.noreply.github.com>
2022-08-11 12:42:49 -0400

    add list sort view

---
+++ Pokedex/SortView.swift
@@ -0,0 +1,40 @@
+//
+//  SortView.swift
+//  Pokedex
+//
+//  Created by Donya Johari on 2022-08-11.
+//
+
+import Foundation
+import SwiftUI
+
+
+struct SortView: View{
+    @State private var dropDownOrder = "Family"
+    var sortItems = ["Family", "Alphabetically"]
+    var body: some View {
+        Menu {
+            
+            Picker("", selection: $dropDownOrder){
+                ForEach(sortItems, id: \.self) {
+                    Text($0).tag(true)
+                }
+            }
+            
+        } label: {
+            Text("Sort")
+        }
+        
+    }
+    
+}
+
+
+
+
+
+struct SortView_Previews: PreviewProvider {
+    static var previews: some View {
+        SortView()
+    }
+}
