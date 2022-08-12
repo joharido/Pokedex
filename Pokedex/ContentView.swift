ba6241090ce87c47ca2a6619b3c256ac68a9f25d
Donya Johari <45051592+joharido@users.noreply.github.com>
2022-08-12 14:10:52 -0400

    add .onchange to pokemon view list to update the view based on sorting order selected

--- Pokedex/ContentView.swift
+++ Pokedex/ContentView.swift
@@ -9,15 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     var pokemonModel = PokemonModel()
+    
+    @State private var defaultPokemon = [Pokemon]()
     @State private var pokemon = [Pokemon]()
+    
+    @State var sortOrder: SortingOrder = .family
+    
     var body: some View {
         NavigationView{
-            List(pokemon.sorted { $0.name < $1.name }){ poke in
+            List(pokemon){ poke in
                 HStack {
                     VStack (alignment: .leading, spacing: 5){
                         Text(poke.name.capitalized).font(.title)
                         Text(poke.type)
-                        //                        Text(poke)
                         Text(poke.description)
                     }
                     AsyncImage(url: URL(string: poke.imgURL)){
@@ -35,27 +39,42 @@ struct ContentView: View {
                             EmptyView()
                         }
                     }
-                    
                 }
-            }.navigationBarTitle("Pokemon")
-                .toolbar {
-                    ToolbarItem(placement: .navigationBarTrailing) {
-                        SortView()
-                    }
-                }
-        }
-        
-        .onAppear{
-            Task{
-                pokemon = try! await pokemonModel.getPokemon()
             }
-            
+            .navigationBarTitle("Pokemon")
+            .onChange(of: sortOrder){ newOrder in
+                switch newOrder{
+                case .alphabetical:
+                    pokemon = defaultPokemon.sorted{$0.name < $1.name}
+                case .family:
+                    pokemon = defaultPokemon
+                }
+                
+            }
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    SortView(currentDropdownState: $sortOrder)
+                }
+            }
+        }
+        .onAppear {
+            Task {
+                defaultPokemon = try! await pokemonModel.getPokemon()
+                pokemon = defaultPokemon
+            }
         }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(sortOrder: .family)
     }
+}
+
+enum SortingOrder: String, CaseIterable, Identifiable  {
+    case alphabetical
+    case family
+    
+    var id: Self { self }
 }
