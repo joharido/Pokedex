b060f161968e28cf15361a45fc42e6b0377efd85
Donya Johari <45051592+joharido@users.noreply.github.com>
2022-08-15 15:04:42 -0400

    Pokemon list view (#1)
    * add list sort view
    
    * add list of pokemons along with their descriptions and pictures
    
    * add .onchange to pokemon view list to update the view based on sorting order selected
    
    * update SortView view
    
    * add pokemon types colors

--- Pokedex/ContentView.swift
+++ Pokedex/ContentView.swift
@@ -9,19 +9,37 @@ import SwiftUI
 
 struct ContentView: View {
     var pokemonModel = PokemonModel()
+    
+    @State private var defaultPokemon = [Pokemon]()
     @State private var pokemon = [Pokemon]()
-     
+    
+    @State var sortOrder: SortingOrder = .family
+    
     var body: some View {
         NavigationView{
-            List(pokemon){
-                poke in Text(poke.name)
+            List(pokemon){ poke in
+                PokemonView(poke: poke)
             }
-            .navigationTitle("Pokemon")
+            .navigationBarTitle("Pokemon")
+            .onChange(of: sortOrder){ newOrder in
+                switch newOrder{
+                case .alphabetical:
+                    pokemon = defaultPokemon.sorted{$0.name < $1.name}
+                case .family:
+                    pokemon = defaultPokemon
+                }
+                
+            }
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    SortView(currentDropdownState: $sortOrder)
+                }
+            }
         }
-        
-        .onAppear{
-            async {
-                pokemon = try! await pokemonModel.getPokemon()
+        .onAppear {
+            Task {
+                defaultPokemon = try! await pokemonModel.getPokemon()
+                pokemon = defaultPokemon
             }
         }
     }
@@ -29,6 +47,15 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(sortOrder: .family)
     }
 }
+
+enum SortingOrder: String, CaseIterable, Identifiable  {
+    case alphabetical
+    case family
+    
+    var id: Self { self }
+}
+
+
